2905940ef7d1f5312e8e8a20ba152f1d527dc323
“Uday <chauhan.s.uday26@gmail.com>
2020-06-27 16:35:36 +0530

    updated portfolio

---
+++ precache-manifest.8f1023cd050a84cd6749e7e6a9a9837b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4e4fce7344bb341b7256",
+    "url": "/static/css/main.26cb3dcb.chunk.css"
+  },
+  {
+    "revision": "4e4fce7344bb341b7256",
+    "url": "/static/js/main.cf47ee0e.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "ab79a467c2f58c99375b",
+    "url": "/static/js/2.b3d50f23.chunk.js"
+  },
+  {
+    "revision": "bb0c6dd2af9f707357fdcfb2990cdf42",
+    "url": "/index.html"
+  }
+];
