4a1c6796809920ebc1a2206c9d31a4fa4cb4c205
uschauha <chauhan.s.uday26@gmail.com>
2020-09-10 22:42:03 +0530

    updated pom

---
+++ README.md
@@ -0,0 +1,19 @@
+<p align="center">
+  <a href="https://ucguy4u.github.io//My-Landing-Page/">
+  </a>
+</p>
+
+<h3 align="center">
+  Minimal Portfolio Page Built with React
+</h3>
+
+
+
+
+# Documentation :
+   [https://github.com/ucguy4u/ucguy4u.github.io/wiki](https://github.com/ucguy4u/ucguy4u.github.io/wiki)
+
+## Demo
+  [https://ucguy4u.github.io/My-Landing-Page/](https://ucguy4u.github.io/) <br/>
+  Note: Click on the page to see Magic
+Please give us a 💖 star 💖 to support us. Thank you.
---
+++ precache-manifest.917ec247d7caf5d3cb9ffa55d2f2edca.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "14d4b71e90502ac9840d",
+    "url": "/ucguy4u.github.io/static/css/main.26cb3dcb.chunk.css"
+  },
+  {
+    "revision": "14d4b71e90502ac9840d",
+    "url": "/ucguy4u.github.io/static/js/main.722f134b.chunk.js"
+  },
+  {
+    "revision": "43094fdbb0f9a397d857",
+    "url": "/ucguy4u.github.io/static/js/runtime~main.08c5bda9.js"
+  },
+  {
+    "revision": "ab79a467c2f58c99375b",
+    "url": "/ucguy4u.github.io/static/js/2.b3d50f23.chunk.js"
+  },
+  {
+    "revision": "f9f88da8234c4fa856b58261953c60f8",
+    "url": "/ucguy4u.github.io/index.html"
+  }
+];
