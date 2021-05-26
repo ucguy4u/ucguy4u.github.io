b8aa3c2651545a144ba7e3ef0ab16bf4c8f0500f
Uday Chauhan <chauhan.s.uday26@gmail.com>
2021-05-26 06:49:34 +0000

    commited

---
+++ docs/precache-manifest.19e95f02f45de22174e3f2214265a0ef.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4206b21e1bf3934daa78c4ca7eb5f5ff",
+    "url": "/index.html"
+  },
+  {
+    "revision": "59f4c9a05e56c2a28b01",
+    "url": "/static/css/main.1db571a8.chunk.css"
+  },
+  {
+    "revision": "2514af890647f5df06a5",
+    "url": "/static/js/2.8eb09a4f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.8eb09a4f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "59f4c9a05e56c2a28b01",
+    "url": "/static/js/main.2c1074ec.chunk.js"
+  },
+  {
+    "revision": "1b0e452c788f293acc2c",
+    "url": "/static/js/runtime-main.a89c3ca2.js"
+  }
+]);
