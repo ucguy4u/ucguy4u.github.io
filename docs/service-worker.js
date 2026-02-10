4c63ef41a673197e1c63e6a35ef5b276a44acc3e
Uday Chauhan <chauhan.s.uday26@gmail.com>
2026-02-10 10:23:55 +0530

    deploy: update docs folder with gradient background build
    - Copy latest build with gradient background to docs folder
    - This is the folder that GitHub Pages deploys from

--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.19e95f02f45de22174e3f2214265a0ef.js"
+  "/ucguy4u.github.io/precache-manifest.6f6f503f0261b357ec75b0844a53c737.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/ucguy4u.github.io/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
